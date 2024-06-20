6cdcace8d561acc381ea356bfc2d48e952a3b1ec
Herold7 <makpier@gmail.com>
2024-06-21 01:15:54 +0200

    Initialisation avec trois routes

--- src/Controller/FavoriteController.php
+++ src/Controller/FavoriteController.php
@@ -2,17 +2,85 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Favorite;
+use App\Entity\Artist;
+use App\Repository\FavoriteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class FavoriteController extends AbstractController
 {
-    #[Route('/favorite', name: 'app_favorite')]
-    public function index(): Response
+    #[Route('/favorites', name: 'favorites', methods: ['GET'])]
+    public function index(FavoriteRepository $favoriteRepository): Response
     {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('favorite/index.html.twig', [
-            'controller_name' => 'FavoriteController',
+            'favorites' => $favoriteRepository->findBy([
+                'user' => $this->getUser()
+            ])
         ]);
+    }
+
+    #[Route('/add-favorite/{artist}', name: 'add_favorite', methods: ['GET'])]
+    public function addFavorite(
+        Artist $artist, 
+        Request $request,
+        EntityManagerInterface $em
+        ): Response
+    {
+        
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $previous = $request->headers->get('referer');
+        $user = $this->getUser();
+        
+        $newFavorite = new Favorite();
+        $newFavorite->setUser($user);
+        $newFavorite->addArtist($artist);
+
+        $user->addFavorite($newFavorite);
+        $em->persist($newFavorite);
+        $em->flush();
+
+        $this->addFlash('success', 'Artist added to favorites successfully.');
+        return $this->redirect($previous);
+    }
+
+    #[Route('/remove-favorite/{Artist}', name: 'remove_favorite', methods: ['GET'])]
+    public function removeFavorite(
+        Request $request,
+        FavoriteRepository $favoriteRepository,
+        EntityManagerInterface $em
+    ): Response
+    {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $previous = $request->headers->get('referer');
+        $user = $this->getUser();
+
+        $favorite = $favoriteRepository->findOneBy([
+            'user' => $user
+        ]);
+
+        if ($favorite) {
+            $user->removeFavorite($favorite);
+            $em->persist($user);
+            $em->remove($favorite);
+            $em->flush();
+            $this->addFlash('success', 'Artist removed from favorites successfully.');
+        }
+
+        // Redirect to the last page visited by the user
+        return $this->redirect($previous);
     }
 }
