43e85db24faf8138274d3fad7e0a90c9b0785528
Herold7 <makpier@gmail.com>
2024-06-21 01:15:09 +0200

    Initialisation du routeur avec une route glable et trois autres spécifique

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -2,17 +2,64 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Artist;
+use App\Repository\ArtistRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArtistController extends AbstractController
+#[Route('/a')] // prefix all artist routes with /a
+class artistController extends AbstractController
 {
-    #[Route('/artist', name: 'app_artist')]
-    public function index(): Response
-    {
+    #[Route('/', name: 'app_artist', methods: ['GET'])]
+    public function index(
+        ArtistRepository $ArtistRepository,
+        PaginatorInterface $paginator,
+        Request $request,
+    ): Response {
+        $pagination = $paginator->paginate(
+            $ArtistRepository->findAll(), // All artists
+            $request->query->getInt('page', 1), // Check page number
+            12 // Items per page
+        );
+
         return $this->render('artist/index.html.twig', [
-            'controller_name' => 'ArtistController',
+            'artists' => $pagination,
+            'userArtists' => $ArtistRepository->findBy(
+                ['host' => $this->getUser()]
+            )
         ]);
     }
+
+    #[Route('/{category}', name: 'app_artist_category', methods: ['GET'])]
+    public function category(
+        ArtistRepository $ArtistRepository,
+        PaginatorInterface $paginator,
+        Request $request,
+    ): Response {
+        $pagination = $paginator->paginate(
+            $ArtistRepository->findBy(['category' => $request->attributes->get('category')]),
+            $request->query->getInt('page', 1),
+            12
+        );
+
+        return $this->render('artist/index.html.twig', [
+            'artists' => $pagination,
+            'userArtists' => $ArtistRepository->findBy(
+                ['host' => $this->getUser()]
+            )
+        ]);
+    }
+
+    #[Route('/details/{id}', name: 'artist', methods: ['GET', 'POST'])]
+    public function details(
+        Artist $artist,
+    ): Response {
+        return $this->render('artist/details.html.twig', [
+            'artist' => $artist,
+        ]);
+    }
+
 }
