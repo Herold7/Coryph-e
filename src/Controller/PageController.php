d39eeee374965bb497b85311f5d262d09d599753
Herold7 <makpier@gmail.com>
2024-06-21 09:31:01 +0200

    Ajout des routes vers les catégory d'artistes

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,55 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ProfileType;
+use App\Service\ProfileService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends AbstractController
 {
-    #[Route('/page', name: 'app_page')]
+    
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('page/index.html.twig', [
             'controller_name' => 'PageController',
         ]);
     }
+
+    #[Route('/chant', name: 'chant', methods: ['GET'])]
+    public function Chant(): Response
+    {
+        return $this->render('page/category.html.twig', [
+            'title' => 'chant',
+            'category' => 'chant',
+            'description' => 'Vous recherchez un chanteur ou une chanteuse pour votre événement ? Vous êtes au bon endroit !',
+        ]);
+    }
+
+    #[Route('/musicien', name: 'musician', methods: ['GET'])]
+    public function Musicien(): Response
+    {
+        return $this->render('page/category.html.twig', [
+            'title' => 'Musicien',
+            'category' => 'musicien',
+            'description' => 'Vous recherchez un musicien pour votre événement ? Vous êtes au bon endroit !',
+        ]);
+    }
+
+    #[Route('/groupe', name: 'groupe', methods: ['GET'])]
+    public function groupe(): Response
+    {
+        return $this->render('page/category.html.twig', [
+            'title' => 'groupe',
+            'category' => 'groupe',
+            'description' => 'Vous recherchez groupe de musique pour votre événement ? Vous êtes au bon endroit !',
+        ]);
+    }
+
+
 }
+
