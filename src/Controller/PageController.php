1603d9910446af3b775c5fe6ee8caacfced7acfd
Herold7 <makpier@gmail.com>
2024-06-25 00:39:21 +0200

    Correction orthographique et mise à jour

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -16,17 +16,18 @@ class PageController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('page/index.html.twig', [
+        return $this->render('page/category.html.twig', [
             'controller_name' => 'PageController',
         ]);
     }
 
-    #[Route('/chant', name: 'chant', methods: ['GET'])]
-    public function Chant(): Response
+    #[Route('/chanteur', name: 'chanteur', methods: ['GET'])]
+    public function Chanteur(): Response
     {
         return $this->render('page/category.html.twig', [
-            'title' => 'chant',
-            'category' => 'chant',
+            'title' => 'chanteur',
+            'category' => 'chanteur',
+            'background' => 'chanteur',
             'description' => 'Vous recherchez un chanteur ou une chanteuse pour votre événement ? Vous êtes au bon endroit !',
         ]);
     }
@@ -37,6 +38,7 @@ class PageController extends AbstractController
         return $this->render('page/category.html.twig', [
             'title' => 'Musicien',
             'category' => 'musicien',
+            'background' => 'musicien',
             'description' => 'Vous recherchez un musicien pour votre événement ? Vous êtes au bon endroit !',
         ]);
     }
@@ -47,6 +49,7 @@ class PageController extends AbstractController
         return $this->render('page/category.html.twig', [
             'title' => 'groupe',
             'category' => 'groupe',
+            'background' => 'groupe',
             'description' => 'Vous recherchez groupe de musique pour votre événement ? Vous êtes au bon endroit !',
         ]);
     }
