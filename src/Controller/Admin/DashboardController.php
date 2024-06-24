76a635bdb6c8b8444ecd8d95d122c5bbf1983e50
Herold7 <makpier@gmail.com>
2024-06-25 00:39:50 +0200

    Mise à jour des icones et de la page de retour

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -56,18 +56,18 @@ class DashboardController extends AbstractDashboardController
     {
         // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-users', User::class);
-        yield MenuItem::linkToCrud('Musiciens', 'fa fa-users-stars', Artist::class);
+        yield MenuItem::linkToCrud('Musiciens', 'fa fa-microphone', Artist::class);
         yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
         yield MenuItem::linkToCrud('Instruments', 'fa fa-guitar', Instrument::class);
         yield MenuItem::linkToCrud('Style musicaux', 'fa fa-headphones', MusicalStyle::class);
-        yield MenuItem::linkToCrud('Prestations', 'fa fa-cubes', Performance::class);
-        yield MenuItem::linkToCrud('Ensemble', 'fa fa-users', Set::class);
+        yield MenuItem::linkToCrud('Prestations', 'fa fa-play', Performance::class);
+        yield MenuItem::linkToCrud('Ensemble', 'fa fa-compact-disc', Set::class);
         yield MenuItem::linkToCrud('Tags', 'fa fa-tags', Tag::class);
-        yield MenuItem::linkToCrud('Plateformes Evenementielles', 'fa fa-location-dot', EventPlatform::class);
-        yield MenuItem::linkToCrud('Plateformes Musicales', 'fa fa-soundcloud', MusicPlatform::class);
-        yield MenuItem::linkToCrud('Réseaux sociaux', 'fa fa-instagram', SocialNetwork::class);
+        yield MenuItem::linkToCrud('Plateformes Evenementielles', 'fa fa-icons', EventPlatform::class);
+        yield MenuItem::linkToCrud('Plateformes Musicales', 'fa-brands fa-soundcloud', MusicPlatform::class);
+        yield MenuItem::linkToCrud('Réseaux sociaux', 'fa fa-podcast', SocialNetwork::class);
     
     // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
-    yield MenuItem::linkToRoute('Retour à l\'accueil', 'fa fa-arrow-left', 'app_page');
+    yield MenuItem::linkToRoute('Retour à l\'accueil', 'fa fa-arrow-left', 'app_home');
     }
 }
