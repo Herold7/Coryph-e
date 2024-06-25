17d45142477c745cc695914ce20604295509a7f7
Herold7 <makpier@gmail.com>
2024-06-25 17:40:03 +0200

    Suppression de code inutiles et traduction en français

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -4,116 +4,112 @@ namespace App\Form;
 
 use App\Entity\User;
 use App\Entity\Favorite;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 
 class ProfileType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('roles', ChoiceType::class, [// champ pour définir le rôle de l'utilisateur
-                'label' => 'Je suis un :',
-                'mapped' => false,
-                'choices' => [
-                    'Artist' => 'user',
-                    'Producer' => 'host',
-                    'Both' => 'host'
-                ],
-                ])
             ->add('name', TextType::class, [// champ pour définir le nom de l'utilisateur
                 'label' => 'Name',
                 'attr' => [
-                    'placeholder' => 'Your name'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Votre nom'
                 ]
             ])
             ->add('corporateName', TextType::class, [// champ pour définir le nom de l'entreprise de l'utilisateur
                 'label' => 'Corporate name',
                 'attr' => [
-                    'placeholder' => 'Your corporate name'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Le nom de votre organisation',
+                    'required' => false
                 ]
             ])
             ->add('siret', TextType::class, [// champ pour définir le numéro de SIRET de l'entreprise de l'utilisateur
                 'label' => 'SIRET',
                 'attr' => [
-                    'placeholder' => 'Your SIRET number'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Son numéro SIRET',
+                    'required' => false,
                 ]
             ])
             ->add('phone', TextType::class, [// champ pour définir le numéro de téléphone de l'utilisateur
                 'label' => 'Phone',
                 'attr' => [
-                    'placeholder' => 'Your phone number'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Votre numéro de téléphone'
                 ]
             ])
             ->add('address', TextType::class, [// champ pour définir l'adresse de l'utilisateur
                 'label' => 'Address',
                 'attr' => [
-                    'placeholder' => 'Your address'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Votre adresse',
+                    'required' => false,
                 ]
             ])
             ->add('additionalAddress', TextType::class, [// champ pour définir l'adresse complémentaire de l'utilisateur
                 'label' => 'Additional address',
                 'attr' => [
-                    'placeholder' => 'Your additional address'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Un complément d\'adresse',
+                    'required' => false,
                 ]
             ])
             ->add('city', TextType::class, [// champ pour définir la ville de l'utilisateur
                 'label' => 'City',
                 'attr' => [
-                    'placeholder' => 'Your city'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Votre ville'
                 ]
             ])
             ->add('zip', TextType::class, [// champ pour définir le code postal de l'utilisateur
                 'label' => 'Zip',
                 'attr' => [
-                    'placeholder' => 'Your zip code'
+                    'class' => 'form-control  mb-2',
+                    'placeholder' => 'Votre code postal'
                 ]
             ])
             ->add('country', TextType::class, [// champ pour définir le pays de l'utilisateur
                 'label' => 'Country',
                 'attr' => [
-                    'placeholder' => 'Your country'
+                    'class' => 'form-control  mb-4',
+                    'placeholder' => 'Votre pays'
                 ]
             ])
-            ->add('consent', CheckboxType::class, [// champ pour définir le consentement de l'utilisateur
-                'label' => 'I agree to the terms and conditions',
-            ])
-            ->add('created_at', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('updated_at', null, [
-                'widget' => 'single_text',
-            ])
+            // ->add('consent', CheckboxType::class, [// champ pour définir le consentement de l'utilisateur
+            //     'label' => 'Je consent à ce que mes données soient utilisées pour les besoins de la plateforme',
+            //     'attr' => [
+            //         'class' => 'form-check-input  mb-4',
+            //     ]
+            // ])
             ->add('image', FileType::class, [
-                'label' => 'Your profile picture',
+                'label' => 'Photo de profil  ',
                 'mapped' => false,
                 'required' => false,
                 'attr' => [
-                    'class' => 'form-control-file mb-2'
+                    'class' => 'form-control-file mb-4'
                 ],
                 'constraints' => [
                     new File([
                         'maxSize' => '1024k',
-                        'maxSizeMessage' => 'The file is too large ({{ size }} {{ suffix }}). Allowed maximum size is {{ limit }} {{ suffix }}.',
+                        'maxSizeMessage' => 'Le fichier est trop gros ({{ size }} {{ suffix }}). La taille maximale est de {{ limit }} {{ suffix }}.',
                         'mimeTypes' => [
                             'image/*',
                         ],
-                        'mimeTypesMessage' => 'Please upload a valid image',
+                        'mimeTypesMessage' => 'Veuillez charger une image valide. Les formats acceptés sont : {{ types }}.',
                     ])
                     ],
                     ])
-            ->add('favorites', EntityType::class, [
-                'class' => Favorite::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
         ;
     }
 
