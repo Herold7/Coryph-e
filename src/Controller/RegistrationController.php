4234735f56a9b4061b566e9563f2258a4ed66bed
Herold7 <makpier@gmail.com>
2024-06-25 17:33:39 +0200

    Traduction des messages en francais. Ajout du choix du rôle dès le register. Modification de la redirection

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -39,6 +39,14 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            // encode the role
+            $role = $form->get('roles')->getData();
+            if ($role === 'artist') {
+                $user->setRoles(['ROLE_MUSICIAN']);
+            } else {
+                $user->setRoles(['ROLE_USER']);
+            }
+
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -47,13 +55,13 @@ class RegistrationController extends AbstractController
                 (new TemplatedEmail())
                     ->from(new Address('admin@coryphee.fr', 'Admin de Coryphée'))
                     ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
+                    ->subject('Veuillez confirmer votre adresse email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('complete_profile');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -76,7 +84,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre adresse email a bien été vérifié .');
 
         return $this->redirectToRoute('complete_profile');
     }
