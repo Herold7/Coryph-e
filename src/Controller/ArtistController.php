85c2795ac438879640210add36d3e6339030721f
Herold7 <makpier@gmail.com>
2024-06-27 00:12:43 +0200

    Mise à jour de la route edit avec la vérification que le musician connecté est autorisé à modifié ou supprimer la fiche artiste. Ajout d'un try catch afin de rediriger l'utilisateur vers la page de tous les artistes. Pour la création prise en compte du musician connecté

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -12,25 +12,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/a')] // prefix all artist routes with /a
+#[Route('/a')] // prefixe toutes les routes par /a
 class ArtistController extends AbstractController
 {
-    #[Route('/', name: 'app_artist_index', methods: ['GET'])]
+    #[Route('/', name: 'app_artist_index', methods: ['GET'])]// Route pour afficher la liste des artistes
     public function index(
         ArtistRepository $artistRepository,
         PaginatorInterface $paginator,
         Request $request
     ): Response {
         $pagination = $paginator->paginate(
-            $artistRepository->findAll(), // All artists
-            $request->query->getInt('page', 1), // Check page number
+            $artistRepository->findAll(), // Tous les artistes
+            $request->query->getInt('page', 1), // Page actuelle
             12 // Items per page
         );
 
         return $this->render('artist/index.html.twig', [
             'artists' => $pagination,
-            'userArtists' => $artistRepository->findBy(
-                ['user' => $this->getUser()]
+            'musicianArtists' => $artistRepository->findBy(
+                ['musician' => $this->getUser()]
             )
         ]);
     }
@@ -50,13 +50,13 @@ class ArtistController extends AbstractController
 
         return $this->render('artist/index.html.twig', [
             'artists' => $pagination,
-            'userArtists' => $artistRepository->findBy(
-                ['user' => $this->getUser()]
+            'musicianArtists' => $artistRepository->findBy(
+                ['musician' => $this->getUser()]
             )
         ]);
     }
 
-    #[Route('/show/{id}', name: 'app_artist_show', methods: ['GET', 'POST'])]
+    #[Route('/show/{id}', name: 'app_artist_show', methods: ['GET', 'POST'])]// Route pour afficher un artiste
     public function show(
         Artist $artist
     ): Response {
@@ -69,13 +69,15 @@ class ArtistController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $artist = new Artist();
+        $artist->setMusician($this->getUser()); // Définir le musicien sur l'utilisateur actuellement connecté
         $form = $this->createForm(ArtistType::class, $artist);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {// Vérifiez si le formulaire a été soumis et est valide
             $entityManager->persist($artist);
             $entityManager->flush();
 
+            $this->addFlash('success', 'La fiche artiste a été créée avec succès.');
             return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -85,32 +87,55 @@ class ArtistController extends AbstractController
         ]);
     }
 
-    #[Route('/edit/{id}', name: 'app_artist_edit', methods: ['GET', 'POST'])]
+    #[Route('/edit/{id}', name: 'app_artist_edit', methods: ['GET', 'POST'])]// Route pour modifier un artiste
     public function edit(Request $request, Artist $artist, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(ArtistType::class, $artist);
-        $form->handleRequest($request);
-    
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-    
+        try {
+            // Vérifiez si le musicien est autorisé à modifier l'artiste
+            if ($artist->getMusician() !== $this->getUser()) {
+                throw $this->createAccessDeniedException('Vous n\'étes pas autorisé à modifier cette fiche artiste.');
+            }
+        } catch (\Exception $e) {// Récupère l'erreur
+            
+            $this->addFlash('error', 'Une erreur s\'est produite lors de la modification de la fiche artiste.');
             return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
         }
-    
+
+        $form = $this->createForm(ArtistType::class, $artist);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La fiche artiste a été modifié avec succès.');
+            return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
+        }
+
         return $this->render('artist/edit.html.twig', [
             'artist' => $artist,
             'form' => $form,
         ]);
     }
 
-    #[Route('/delete/{id}', name: 'app_artist_delete', methods: ['POST'])]
+    #[Route('/delete/{id}', name: 'app_artist_delete', methods: ['GET','POST'])]// Route pour supprimer un artiste
     public function delete(Request $request, Artist $artist, EntityManagerInterface $entityManager): Response
     {
+        try {
+            // Check if the musician is authorized to delete the artist
+            if ($artist->getMusician() !== $this->getUser()) {
+                throw $this->createAccessDeniedException('Vous n\'étes pas autorisé à supprimer cette fiche artiste.');
+            }
+        } catch (\Exception $e) {
+            
+            $this->addFlash('error', 'Une erreur s\'est produite lors de la suppression de la fiche artiste.');
+            return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
+        }
+
         if ($this->isCsrfTokenValid('delete'.$artist->getId(), $request->request->get('_token'))) {
             $entityManager->remove($artist);
             $entityManager->flush();
         }
-
+        $this->addFlash('success', 'L\'artiste a été supprimé avec succès.');
         return $this->redirectToRoute('app_artist_index', [], Response::HTTP_SEE_OTHER);
     }
 }
