954e880d1be9f81a0c6b24706babceec676e4c16
Herold7 <makpier@gmail.com>
2024-07-01 22:03:34 +0200

    redirection vers la page d'accueil

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -50,7 +50,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('chanteur'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
