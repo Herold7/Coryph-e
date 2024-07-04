434887b832e6a7c3fa2ba8dfc300ff3f2ec2c532
Herold7 <makpier@gmail.com>
2024-07-04 16:50:17 +0200

    Ajout de commentaires

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -8,6 +8,10 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Review>
+ * @method Review|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Review|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Review[]    findAll()
+ * @method Review[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ReviewRepository extends ServiceEntityRepository
 {
